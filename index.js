cc5fade71373983d686db2ecfbdf2ac23969b904
ayaanqui <moahammedayaan.dev@gmail.com>
2020-08-04 21:30:41 -0500

    Cleared all files

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const app = express();
-const port = 3000;
-
-app.get('/', (_, res) => res.send({ message: 'Lhintaro my boi wassup!!' }));
-
-app.listen(port, () => console.log(`\nApp running at http://localhost:${port}\n\n`));

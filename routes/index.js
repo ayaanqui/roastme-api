67c1ffdb058a0afad58948100dc3c2e3856db6cd
ayaanqui <moahammedayaan.dev@gmail.com>
2020-09-04 17:16:13 -0500

    Added /roasts to router

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('', (_, res) => res.send('Hello, welcome to the RoastMe api'));
-router.use('/auth', require('./auth'))
+router.use('/auth', require('./auth'));
+router.use('/roasts', require('./roasts'));
 
 module.exports = router;
